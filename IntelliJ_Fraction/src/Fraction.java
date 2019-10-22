e65938e1a3ffe79c6961c2049bbdc2e48ec53dce
Haoye <haoyetang@gmail.com>
2019-10-22 17:06:38 -0400

    Initial commit

---
+++ IntelliJ_Fraction/src/Fraction.java
@@ -0,0 +1,112 @@
+/**
+ * Represents a fraction
+ *
+ * @author Haoye
+ * @version 1571770883
+ */
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+    private Fraction result;
+
+    public Fraction() {
+        numerator = 0;
+        denominator = 1;
+    }
+
+    public Fraction(int numeratorIn, int denominatorIn) {
+        numerator = numeratorIn;
+        denominator = denominatorIn;
+    }
+
+    public Fraction(Fraction fractionIn) {
+        numerator = fractionIn.numerator;
+        denominator = fractionIn.numerator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setNumerator(int newNumerator) {
+        numerator = newNumerator;
+    }
+
+    public void setDenominator(int newDenominator) {
+        denominator = newDenominator;
+    }
+
+    public Fraction add(int factor) {
+        result = new Fraction(numerator + (factor * denominator), denominator);
+        return result;
+    }
+
+    public Fraction add(Fraction fractionIn) {
+        if (denominator == fractionIn.denominator) {
+            result = new Fraction(numerator + fractionIn.numerator, denominator);
+        } else {
+            result = new Fraction(numerator * fractionIn.denominator + fractionIn.numerator * denominator, denominator * fractionIn.denominator);
+        }
+        return result;
+    }
+
+    public Fraction subtract(int factor) {
+        result = new Fraction(numerator - (factor * denominator), denominator);
+        return result;
+    }
+
+    public Fraction subtract(Fraction fractionIn) {
+        if (denominator == fractionIn.denominator) {
+            result = new Fraction(numerator - fractionIn.numerator, denominator);
+        } else {
+            result = new Fraction(numerator * fractionIn.denominator - fractionIn.numerator * denominator, denominator * fractionIn.denominator);
+        }
+        return result;
+    }
+
+    public Fraction multiply(int factor) {
+        result = new Fraction(numerator * factor, denominator);
+        return result;
+    }
+
+    public Fraction multiply(Fraction fractionIn) {
+        result = new Fraction(numerator * fractionIn.numerator, denominator * fractionIn.denominator);
+        return result;
+    }
+
+    public Fraction divide(int factor) {
+        result = new Fraction(numerator, denominator * factor);
+        return result;
+    }
+
+    public Fraction divide(Fraction fractionIn) {
+        result = new Fraction(numerator * fractionIn.denominator, denominator * fractionIn.numerator);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public double getValue() {
+        return (double)numerator/denominator;
+    }
+
+    public int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+
+    public int lcm(int a, int b)
+    {
+        return (a*b)/gcd(a, b);
+    }
+}
---
+++ IntelliJ_Fraction/src/UsesFraction.java
@@ -0,0 +1,117 @@
+import java.util.Scanner;
+
+public class UsesFraction {
+    private static int numerator;
+    private static int denominator;
+    private static int number;
+    private static char character;
+
+    public static void main(String[] arg) {
+        System.out.println("Hi, Soyboy!");
+
+        System.out.println("Would you like to perform aliasFun()? (Y/N)");
+        takeInput("char");
+        if (character == 'y') {
+            aliasFun();
+            return;
+        }
+
+        takeInput("frac");
+        Fraction f = new Fraction(numerator, denominator);
+
+        System.out.println("Would you like to perform operations on your fraction? (Y/N)");
+        takeInput("char");
+        if (character == 'y') {
+            System.out.println("Would you like to perform what operation? (Add/Subtract/Multiplication/Division)");
+            takeInput("char");
+            Fraction result = null;
+            switch (character) {
+                case 'a':
+                    System.out.println("Would you like to add an integer or a fraction? (I/F)");
+                    takeInput("char");
+                    if (character == 'i') {
+                        System.out.println("How much would you like to add?");
+                        takeInput("int");
+                        result = f.add(number);
+                    } else {
+                        takeInput("frac");
+                        result = f.add(f);
+                    }
+                    break;
+                case 's':
+                    System.out.println("Would you like to subtract an integer or a fraction? (I/F)");
+                    takeInput("char");
+                    if (character == 'i') {
+                        System.out.println("How much would you like to subtract?");
+                        takeInput("int");
+                        result = f.subtract(number);
+                    } else {
+                        takeInput("frac");
+                        result = f.subtract(f);
+                    }
+                    break;
+                case 'm':
+                    System.out.println("Would you like to multiply by an integer or a fraction? (I/F)");
+                    takeInput("char");
+                    if (character == 'i') {
+                        System.out.println("By what factor would you like to multiply?");
+                        takeInput("int");
+                        result = f.multiply(number);
+                    } else {
+                        takeInput("frac");
+                        result = f.multiply(f);
+                    }
+                    break;
+                case 'd':
+                    System.out.println("Would you like to divide by an integer or a fraction? (I/F)");
+                    takeInput("char");
+                    if (character == 'i') {
+                        System.out.println("By what factor would you like to divide?");
+                        takeInput("int");
+                        result = f.divide(number);
+                    } else {
+                        takeInput("frac");
+                        result = f.divide(f);
+                    }
+                    break;
+            }
+            System.out.println("Your final fraction is: " + result.toString());
+            System.out.println("And its " + result.getValue() + " represented in decimal");
+        } else {
+            System.out.println("Your final fraction is: " + f.toString());
+            System.out.println("And its " + f.getValue() + " represented in decimal");
+        }
+    }
+
+    public static void aliasFun () {
+        Fraction f = new Fraction(2, 3);
+        System.out.println(f.toString());
+        Fraction result1 = f.multiply(7);
+        Fraction result2 = new Fraction(result1);
+        System.out.println(result1);
+        System.out.println(result2);
+        result1.setNumerator(11);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    public static void takeInput(String type) {
+        if (type == "frac") {
+            Scanner numeratorToSet = new Scanner(System.in);
+            Scanner denominatorToSet = new Scanner(System.in);
+
+            System.out.print("Set your numerator: ");
+            numerator = numeratorToSet.nextInt();
+            System.out.print("Set your denominator: ");
+            denominator = denominatorToSet.nextInt();
+        }
+        if (type == "int") {
+            Scanner factorToSet = new Scanner(System.in);
+            number = factorToSet.nextInt();
+        }
+        if (type == "char") {
+            Scanner initialCap = new Scanner(System.in);
+            character = initialCap.next().charAt(0);
+        }
+    }
+}
