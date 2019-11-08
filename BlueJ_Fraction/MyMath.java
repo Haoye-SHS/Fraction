bf0b7d28505f9f03ee9d6f880c75c6e4b339e0ac
Haoye Tang <haoyetang@gmail.com>
2019-11-07 23:29:03 -0500

    Copy progress to BlueJ

--- BlueJ_Fraction/MyMath.java
+++ BlueJ_Fraction/MyMath.java
@@ -1,4 +1,16 @@
+/**
+ * this is a math class whose sole purpose is to calculate the greatest common factor of two input integers
+ *
+ * @author Haoye
+ * @version 1573186958
+ */
+
 public class MyMath {
+    /**
+     * @param a is the first integer input
+     * @param b is teh second integer input
+     * @return the gcf of input a and b
+     */
     public static int gcf(int a, int b) {
         if (a == 0) return Math.abs(b);
         if (b == 0) return Math.abs(a);
